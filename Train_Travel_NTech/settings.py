7f2c48fec70b6b02d1a7c858d128789e6f41a243
neeleshrajpoot2351 <neeleshrajpoot2351@gmail.com>
2025-01-20 17:50:02 +0530

    updating setting.py file

--- Train_Travel_NTech/settings.py
+++ Train_Travel_NTech/settings.py
@@ -41,10 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Train_Travel_app',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Train_Travel_NTech.urls'
