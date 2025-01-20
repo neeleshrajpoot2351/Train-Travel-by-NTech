9fdeba14014637a1c65c29fa461899287ba6b955
neeleshrajpoot2351 <neeleshrajpoot2351@gmail.com>
2025-01-20 20:17:44 +0530

    This is add only database

--- Train_Travel_NTech/settings.py
+++ Train_Travel_NTech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key')
 
 # Debug
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -83,23 +84,12 @@ WSGI_APPLICATION = 'Train_Travel_NTech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600, ssl_require=True),
 }
+
 
 
 
