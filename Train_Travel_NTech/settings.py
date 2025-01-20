044ebd3ac9ca49bfe0cc975ff190be0ce522c91d
neeleshrajpoot2351 <neeleshrajpoot2351@gmail.com>
2025-01-20 20:46:44 +0530

    akjghs

--- Train_Travel_NTech/settings.py
+++ Train_Travel_NTech/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS':{'sslmode':'require'},
     }
 }
 
