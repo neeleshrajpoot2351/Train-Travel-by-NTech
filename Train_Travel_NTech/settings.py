7552aaf117e5db77822334cac9b1e6d0f3b30743
neeleshrajpoot2351 <neeleshrajpoot2351@gmail.com>
2025-01-21 13:29:41 +0530

    dfshfa

--- Train_Travel_NTech/settings.py
+++ Train_Travel_NTech/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', default=5432), # Default PostgreSQL port
+        'PORT': os.getenv('DB_PORT', default=55120), # Default PostgreSQL port
     }
 }
 
