96f60e330d5c6054517144fee90fd3f5e993a4b2
neeleshrajpoot2351 <neeleshrajpoot2351@gmail.com>
2025-01-21 13:20:38 +0530

    afhsd

--- Train_Travel_NTech/settings.py
+++ Train_Travel_NTech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Secret Key
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['*']
 
